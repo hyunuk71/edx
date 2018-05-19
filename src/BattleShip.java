b797e5ed06190b794ea8dddb837f302319d75d51
hyunuk71 <hyunuk71@users.noreply.github.com>
2018-05-19 00:38:59 -0700

    Initial

---
+++ src/BattleShip.java
@@ -0,0 +1,19 @@
+public class BattleShip {
+
+    public static void main(String[] args) {
+        opening();
+    }
+
+    public static void opening() {
+        final int das = 231; /* Set count to 0 */
+        int scnt = 0; // Set count to 0
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/BattleShipMap.java
@@ -0,0 +1,5 @@
+public class BattleShipMap {
+    private int[][] map = new int[10][10];
+
+
+}
